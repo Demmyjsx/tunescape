2a867adbd61c8d64d9b6ea0efe47ab561377a89e
Demmyjsx <ademolaabds@gmail.com>
2025-10-03 19:47:35 +0100

    fixed a bug error

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -1,7 +1,7 @@
 
 export default function Footer() {
   return (
-    <footer className="bg-black text-gray-400 text-sm py-6 mt-10 border-t border-gray-700">
+    <footer className="bg-black text-gray-400 text-sm py-6 border-t border-gray-700">
       <div className="max-w-6xl mx-auto px-4 flex flex-col md:flex-row items-center justify-between">
         
         <p className="mb-2 md:mb-0">
